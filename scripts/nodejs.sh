206970c443a61aa924cc3c1a3d68a73c5e14d042
Alberto Castañeiras <alberto@casta.me>
2023-02-27 20:12:55 +0100

    Add nodejs

---
+++ scripts/nodejs.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+#
+# Installs or updates nodejs.
+
+# Global variables
+# -----------------------------------------------------------------------------
+SCRIPT_NAME="$(basename ${0})"
+SRC_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)
+NVM_VERSION=""
+
+# Loggers
+# -----------------------------------------------------------------------------
+info() { printf "%b[info]%b %s\n" '\e[0;32m\033[1m' '\e[0m' "$*" >&2; }
+warn() { printf "%b[warn]%b %s\n" '\e[0;33m\033[1m' '\e[0m' "$*" >&2; }
+erro() { printf "%b[erro]%b %s\n" '\e[0;31m\033[1m' '\e[0m' "$*" >&2; exit 1; }
+
+# Install nvm
+# -----------------------------------------------------------------------------
+function install_nvm () {
+    curl -o- -s https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash &>/dev/null || \
+    return 1 && return 0
+}
+
+# Get nvm lastest version
+# -----------------------------------------------------------------------------
+function get_nvm_version () {
+    NVM_VERSION=$(curl --silent "https://api.github.com/repos/nvm-sh/nvm/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    if [[ -z "${NVM_VERSION}" ]]; then
+        return 1
+    fi
+    return 0
+}
+
+# Install nodejs
+# -----------------------------------------------------------------------------
+function install_nodejs () {
+    if ! command -v nvm &>/dev/null; then
+        NVM_DIR="$([[ -z "${XDG_CONFIG_HOME-}" ]] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+        [[ -s "${NVM_DIR}/nvm.sh" ]] && . "${NVM_DIR}/nvm.sh"
+        echo &>/dev/null
+    fi
+    nvm install --lts &>/dev/null || \
+    return 1 && return 0
+}
+
+# Main
+# -----------------------------------------------------------------------------
+function main () {
+    get_nvm_version || erro "get latest nvm version"
+    install_nvm || erro "install nvm"
+    install_nodejs || erro "install nodejs"
+    info "nodejs install done ✅"
+}
+
+main "$@"
--- zshrc
+++ zshrc
@@ -29,6 +29,11 @@ fi
 # -----------------------------------------------------------------------------
 [[ -f "/usr/bin/terraform" ]] && complete -C /usr/bin/terraform terraform
 
+# Load nvm
+# -----------------------------------------------------------------------------
+export NVM_DIR="$([[ -z "${XDG_CONFIG_HOME-}" ]] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[[ -s "${NVM_DIR}/nvm.sh" ]] && . "${NVM_DIR}/nvm.sh"
+
 # Load GPG
 # -----------------------------------------------------------------------------
 export GPG_TTY=$(tty)
